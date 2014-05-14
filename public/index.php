78f4ed0dc1817bf1441fe003b800538a2e44836e
k-r-ll <ndsdmfwg@gmail.com>
2014-05-14 22:20:12 +0400

    second commit

--- public/index.php
+++ public/index.php
@@ -7,4 +7,19 @@ $app->get('/', function () {
     readfile('index.html');
 });
 
+$app->get('/install/:host/:dbname/:username/:password', function ($host, $dbname, $username, $password) {
+	ORM::configure("mysql:host={$host};dbname={$dbname}");
+	ORM::configure('username', $username);
+	ORM::configure('password', $password);
+
+	$db = ORM::get_db();
+	$db->exec("
+	        CREATE TABLE IF NOT EXISTS users (
+	            id SMALLINT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+	            login VARCHAR(20), 
+	            password VARCHAR(40) 
+	        );"
+	    );
+});
+
 $app->run();
